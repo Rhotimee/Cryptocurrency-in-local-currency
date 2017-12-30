505906c351044fc1cbd8760faa6a0fcdfca490ea
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2017-12-30 04:30:42 +0100

    chaining callbacks

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const yargs = require('yargs');
 
-const crypto = require('./Crypto-price/crypto')
+const crypto = require('./Crypto-price/crypto');
+const local = require('./local-currency/local');
 
 
 const argv = yargs
@@ -24,12 +25,23 @@ let toVerify = (arg) => {
 
 command = toVerify(firstArg);
 countryCode = toVerify(secondArg);
+code = 'USD'+ toVerify(secondArg);
 
+
+let cryptoPrice, localPrice; 
 
 crypto.getCrypto(command).then((msg) => {
-    console.log(`Name: ${msg.name}`);
-    console.log(`Current Price: $${msg.currentPrice} USD`);
-    console.log(`Market Cap: $${msg.marketCap} USD`);
+    cryptoPrice = msg.currentPrice;
+    console.log(`1 ${command} = $${msg.currentPrice} USD`);
+    local.getLocalCurrency(code).then((msg) => {
+        localPrice = msg;
+        console.log(`1 USD = ${msg} ${countryCode}`)
+        console.log(`1 ${command} = ${cryptoPrice*localPrice} ${countryCode}`)
+    }, (errorMessage) => {
+        console.log(errorMessage)
+    });
+    
 }, (errorMessage) => {
     console.log('Error: ', errorMessage)
 })
+
---
+++ local-currency/local.js
@@ -0,0 +1,27 @@
+const request = require('request');
+
+let getLocalCurrency = (realCode) => {
+    
+    return new Promise((resolve, reject) => {
+        request({
+            url: `http://www.apilayer.net/api/live?access_key=a1c2abd1172003288d57872b5da731c4`,
+            json: true
+        }, (error, response, body) => {
+            // resolve(body.quotes.USDNGN)
+            if (!error && response.statusCode === 200){
+                // resolve(body.quotes.realCode)
+                // reject ('cannot find')
+                if (body.quotes[realCode]){
+                    resolve(body.quotes[realCode])
+                } reject ('cannot find')
+            } reject ('cannot connect to server')
+        }
+    )
+    })
+
+}
+
+
+module.exports = {
+    getLocalCurrency
+}
