8a272f08198dba4679c9da554baa054631acc011
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2017-12-30 02:50:56 +0100

    refactoring & create file for local-currency

--- app.js
+++ app.js
@@ -25,42 +25,6 @@ let toVerify = (arg) => {
 command = toVerify(firstArg);
 countryCode = toVerify(secondArg);
 
-// if (typeof x === 'string'){
-//     command = x.toUpperCase();
-//     // command2 = x.toLowerCase() ;
-// } else {
-//     command = ''+x;
-// }
-
-// let cryp = () => {
-//     return new Promise ((resolve, reject) => {
-
-//         request({
-//             url: `https://api.coinmarketcap.com/v1/ticker/`,
-//             json: true
-//         }, (error, response, body) => {
-        
-//             if (!error && response.statusCode === 200){
-//                 for (let i = 0; i < body.length; i++){
-//                     if (body[i].symbol === command || body[i].id === command2){
-//                         resolve({
-//                             name: body[i].name,
-//                             currentPrice: body[i].price_usd,
-//                             marketCap: body[i].market_cap_usd
-//                         });
-//                         // console.log(`Name: ${body[i].name}`);
-//                         // console.log(`Current Price: $${body[i].price_usd} USD`);
-//                         // console.log(`Market Cap: $${body[i].market_cap_usd} USD`);
-//                         break;
-//                     } else {
-//                         reject(`${argv._[0]} could not be found`); break;
-//                     }
-//                 } 
-//             } else {reject(`The server could not be reached`)}   
-//         });
-
-//     })
-// }
 
 crypto.getCrypto(command).then((msg) => {
     console.log(`Name: ${msg.name}`);
